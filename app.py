ecd359415ee0483d4ce89b162d16437de4e46d86
rattanakopanukul-del <rattana.kopanukul@gmail.com>
2025-10-19 14:03:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 
 # Load the trained regression model
-with open("model-regุ-67130701918.pkl", "rb") as file:
+with open("model-reg-67130701918.pkl", "rb") as file:
     model = pickle.load(file)
 
 st.title("Sales Prediction App")
